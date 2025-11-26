a0dd5a79ed6bcc69da6fce7ceb34777c4b23838c
Adriaan Zwemer <zwem0022@hz.nl>
2025-11-26 13:55:12 +0100

    voeg voorbeelt test toe met mocha

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import assert from "assert";
+//voorbeeld test
+describe("Array", function () {
+  describe("#voorbeeld test", function () {
+    it("2 + 2 is gelijk aan 4", function () {
+      assert.equal(2 + 2, 4);
+    });
+  });
+});
