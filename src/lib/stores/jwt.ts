70eed9afdb81b58d1d383da43f918e81a67dc3f6
Adriaan Zwemer <zwem0022@hz.nl>
2025-12-23 11:47:42 +0100

    fix cookie expiry

--- src/lib/stores/jwt.ts
+++ src/lib/stores/jwt.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
+import { getPayload } from '$lib/Auth';
 let defaultJwt = undefined;
 if(browser){
     const cookie = await window.cookieStore.get("jwt");
@@ -10,5 +11,16 @@ if(browser){
 export const jwt = writable<string|undefined>(defaultJwt);
 
 jwt.subscribe(async newJwt=>{
-    if(browser) await window.cookieStore.set("jwt", newJwt ?? "");
+    if(browser) {
+        if(newJwt) {
+            const payload = getPayload(newJwt);
+            await window.cookieStore.set({
+                name: "jwt", 
+                value: newJwt ?? "",
+                expires: payload.exp * 1000
+            });
+        } else {
+            await window.cookieStore.delete("jwt")
+        }
+    }
 });
